9c89d714f4d374d69ade5cf062f7b2addcafd411
udatIT2005 <datung501@gmail.com>
2026-03-17 13:55:35 +0700

    Ôn lại cấu trúc điều kiện

---
+++ JavaScript/Day02/main.js
@@ -0,0 +1,12 @@
+function swapNumber(a, b) {
+  a = a + b;
+
+  b = a - b;
+
+  a = a - b;
+  return [a, b];
+}
+console.log(swapNumber(3, 5));
+/**
+ * Lập trình là giải quyêt bài của cuộc sống 
+ */
---
+++ JavaScript/Day02/main2.js
@@ -0,0 +1,25 @@
+/**
+ * @ for
+ * Bước 1 : Khởi tạo biến đêm
+ * Bước 2 : Kiểm tra điều kiện
+ * Bước 3 : Thực hiện công việc khi điều kiện đúng thoát khỏi điều kiện sai
+ * Bước 4 : Thực hiện bước nhảy
+ *
+ * Bài tập : Thực hiện nhập 1 số từ n nhập n từ phím chia hết cho 5
+ */
+function result(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 == 0) {
+      console.log(i);
+    }
+  }
+}
+let n = parseInt(prompt("Nhập N nguyên dương"));
+result(n);  
+
+// Cấu trúc các điều kiện 
+/** 
+ * 1. if else
+ * 2. switch case
+ * 3. do while
+ */
